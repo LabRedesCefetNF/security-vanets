7221760e47622c8bb95ec9e89125f0c7c8c69c73
Nilson Mori <nilsonmori@yahoo.com>
2021-05-19 21:06:28 -0300

    transmissão e criptografia só com o Arduino MEGA

--- transmitter-example/transmitter-example.ino
+++ transmitter-example/transmitter-example.ino
@@ -3,27 +3,59 @@
 #include "securino.h"
 #include "Javino.h"
 
-// TRANSMITTER
+Javino javino;
+AsciiHex ah;
+Securino securino;
+
+  byte byteMsg[] = {0x4d,0x65,0x20,0x61,0x6a,0x75,0x64,0x61,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
+  byte byteChave[] = {0x2b,0x7e,0x15,0x16,0x28,0xae,0xd2,0xa6,0xab,0xf7,0x15,0x88,0x09,0xcf,0x4f,0x3c};
+  byte * byteCifrado;
+  byte * charDecifrado;
+
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  javino.enableRF(12,11);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  AsciiHex ah;
-  Securino sec;
-  /*Javino jav;
+    delay(3000);
+  String strMsg = "segredo";
+  /*Enviano mensagem em claro*/
+    javino.sendMsgRF(strMsg);
+    delay(1000);
+  /****************************/
+  
+  Serial.print("PLAIN:");
+  exibe(byteMsg,16);
+  
+  byteCifrado = securino.encript("aes-128-ecb", byteMsg, byteChave);
+  Serial.print("ECB: ");
+  exibe(byteCifrado,16);
+  delay(3000);
 
-  jav.enableRF(12,11);
-  jav.sendMsgRF("teste1");
-  delay(500);*/
+  byteCifrado = securino.encript("aes-128-cbc", byteMsg, byteChave);
+  Serial.print("CBC: ");
+  exibe(byteCifrado,32);
+  
 
+
+  
+//  Serial.println(byteCifrado);
+
+ // decifrado = securino.decript("aes-128-cbc", cifrado, chave);
+
+ // securino.show("Chave ", chave);
+ // Serial.println();
+//  securino.show("Cifrado ", cifrado);
+ // Serial.println();
+//  securino.show("Decifrado ", decifrado);
+  
+/*
   char msg[] = "Bom dia, Patrick"; //16 bytes
   char enviar[] = "                ";
   byte hex[] = {0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
-  byte chave[] = {0x2b,0x7e,0x15,0x16,0x28,0xae,0xd2,0xa6,0xab,0xf7,0x15,0x88,0x09,0xcf,0x4f,0x3c};
+  
   char encoded[BASE64::encodeLength(16)];
 
   byte * cifrado;
@@ -45,7 +77,13 @@ void loop() {
   Serial.print("Mensagem cifrada (base64): ");
   Serial.println(encoded);
   Serial.println();
+/*  */
+}
 
-  /*jav.sendMsgRF("teste2");
-  delay(500);*/
+void exibe(byte * in, int x){
+  for(int i=0; i < x; i++){
+      Serial.print(in[i], HEX);
+      Serial.print(" ");  
+   }
+   Serial.println();  
 }
