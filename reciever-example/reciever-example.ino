6a70681092c43ffdea6e3c77e3ac8ab65bc6d1e2
Nilson Mori <nilsonmori@yahoo.com>
2021-05-21 20:01:40 -0300

    comentários

--- reciever-example/reciever-example.ino
+++ reciever-example/reciever-example.ino
@@ -1,21 +1,36 @@
-#include "asciiHex.h"
 #include "securino.h"
 #include "Javino.h"
+#include "Konverter.h"
+Javino javino;
+Securino securino;
+Konverter konverter;
 
-// RECIEVER
+String strMsg;
+byte * byteMsg;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  
+    javino.enableRF(12,11);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Javino j;
-  Securino s;
-  j.enableRF(12,11);
-  if(j.availableMsgRF()){
-    Serial.println(j.getMsg());
+  if(javino.availableMsgRF()){
+    strMsg = javino.getMsg();
+    Serial.println(strMsg);
+    konverter.strB64toByte(strMsg,byteMsg);
+    Serial.print("CIFRADA: ");
+    exibe(byteMsg,16);
   }
+  
+}
+
+
+
+/*****Auxiliar *****/
+void exibe(byte * in, int x){
+  for(int i=0; i < x; i++){  
+      Serial.print(in[i],HEX);
+      Serial.print(" ");  
+   }
+   Serial.println();  
 }
