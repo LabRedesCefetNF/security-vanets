a16236cb840e98f94c3a321704f281875a9401e0
Nilson Mori <nilsonmori@yahoo.com>
2021-08-26 10:40:33 -0300

    experimentos

--- src/others/experimentos/veiculo/veiculo.ino
+++ src/others/experimentos/veiculo/veiculo.ino
@@ -1,4 +1,3 @@
-#include "Javino.h"
 void setup() {
   Serial.begin(9600);
   enableRF(12,11);
@@ -6,10 +5,19 @@ void setup() {
 }
 
 void loop() {
-
   if(receiver()){
     Serial.print("Rcpt:");
-    Serial.println(getPlainTxt());
+    Serial.print(getPlainTxt());
+    action(getPlainTxt());
   }
+}
 
+void action(String act){
+  if(act=="command=go ahead"){
+    Serial.println("  (Start [OK])");
+  }else if(act=="command=stop now"){
+    Serial.println("  (Stop  [OK])");
+  }else{
+    Serial.println(" (CMD [ERROR])");
+  }
 }
